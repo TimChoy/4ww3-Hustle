1aedde0f32cbca3a169dbf4fe8790993d8c8cde9
Tim Choy <timchoy99@gmail.com>
2021-10-06 22:33:22 -0400

    Finish up homepage and add links

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Navigation from './Components/Navigation.js';
 import { Switch, Route } from 'react-router-dom';
 import Hustle from './Components/Hustle';
 import Search from './Components/Search';
+import Contribute from './Components/Contribute';
+import Login from './Components/Login';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navigation />
       <Switch>
-      <Route exact path='/' component={Hustle} />
-      <Route path='/search' component={Search} />
+        <Route exact path='/' component={Hustle} />
+        <Route path='/search' component={Search} />
+        <Route path='/contribute' component={Contribute} />
+        <Route path='/login' component={Login} />
       </Switch>
     </div>
   );
