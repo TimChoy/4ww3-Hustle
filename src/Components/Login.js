489e9a8d9d55f04bc40214039e851ef3941e28bb
Tim Choy <timchoy99@gmail.com>
2021-10-06 23:21:02 -0400

    Create login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,48 @@
+import { Button, FloatingLabel, Form, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../Styles/Hustle.css';
+import '../Styles/Login.css';
+
 function Login() {
     return (
-        <div>Hello World</div>
+        <div className="Hustle">
+            <div className="Input"> {/* TODO: Use Formik for validation */}
+                <div>
+                    <h2>Register</h2>
+                </div>
+                <Form>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="registerFName">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control placeholder="First Name" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="registerLName">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control placeholder="Last Name" />
+                        </Form.Group>
+                    </Row>
+                    
+                    <Form.Group className="mb-3" controlId="registerEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="registerPass">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control placeholder="New password" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" id="registerCheckbox">
+                        <Form.Check type="checkbox" label="I agree to the terms and conditions" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Register
+                    </Button>
+                </Form>
+            </div>
+        </div>
     );
 }
 
