519992143fc567639bb8aaa6707a11d4e271e30d
Tim Choy <timchoy99@gmail.com>
2021-11-04 02:40:26 -0400

    Add form validation to register

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,56 +1,144 @@
 import { Button, Col, Form, Row } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import FooterNav from './Footer'
 import '../Styles/Hustle.css';
 import '../Styles/Login.css';
 
+const schema = Yup.object().shape({
+  firstName: Yup.string().required('Required'),
+  lastName: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid Email').required('Required'),
+  password: Yup.string().required('Required'),
+  terms: Yup.bool().required().oneOf([true], 'Terms must be accepted'),
+});
+
 function Login() {
-    const history = useHistory();
-    const handleOnClick = () => history.push('/');
+  const history = useHistory();
+  const handleOnSubmit = (input) => { {/* Handles form submission, once validation is complete */}
+    console.log(input);
+    history.push('/');
+  }
 
-    return (
-        <div className="Login" >
-            <div className="Input" > {/* TODO: Use Formik for validation */}
-                <div>
-                    <h2> Register </h2>
-                </div>
-                <Form> { /* Form for registration */}
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="registerFName">
-                            <Form.Label>First Name</Form.Label>
-                            <Form.Control placeholder="First Name" />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="registerLName">
-                            <Form.Label>Last Name</Form.Label>
-                            <Form.Control placeholder="Last Name" />
-                        </Form.Group>
-                    </Row>
-
-                    <Form.Group className="mb-3" controlId="registerEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Email" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="registerPass">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control placeholder="New password" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" id="registerCheckbox">
-                        <Form.Check type="checkbox" label="I agree to the terms and conditions" />
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit" onClick={handleOnClick}>
-                        Register
-                    </Button>
-                </Form>
-            </div>
-            <div className="Fixed-bottom">
-                <FooterNav />
-            </div>
+  return (
+    <div className="Login">
+      <div className="Input">
+        <div>
+          <h2> Register </h2>
         </div>
-    );
+        <Formik
+          validationSchema={schema}
+          onSubmit={handleOnSubmit}
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            terms: false,
+          }}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            values,
+            touched,
+            errors,
+          }) => (
+            <Form noValidate onSubmit={handleSubmit}> { /* Form for registration */}
+              <Row className="mb-2">
+                <Form.Group as={Col} controlId="registerFName">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="firstName"
+                    value={values.firstName}
+                    onChange={handleChange}
+                    isValid={touched.firstName && !errors.firstName}
+                    isInvalid={!!errors.firstName}
+                    placeholder="First Name"
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.firstName}
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="registerLName">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="lastName"
+                    value={values.lastName}
+                    onChange={handleChange}
+                    isValid={touched.lastName && !errors.lastName}
+                    isInvalid={!!errors.lastName}
+                    placeholder="Last Name"
+                  />
+
+                  <Form.Control.Feedback type="invalid">
+                    {errors.lastName}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-3" controlId="registerEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  isValid={touched.email && !errors.email}
+                  isInvalid={!!errors.email}
+                  placeholder="Email"
+                />
+
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="registerPass">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  isValid={touched.password && !errors.password}
+                  isInvalid={!!errors.password}
+                  placeholder="New password"
+                />
+
+                <Form.Control.Feedback type="invalid">
+                  {errors.password}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3" id="registerCheckbox">
+                <Form.Check
+                  required
+                  name="terms"
+                  label="I agree to the terms and conditions"
+                  onChange={handleChange}
+                  isInvalid={!!errors.terms}
+                  feedback={errors.terms}
+                  feedbackType="invalid"
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+      <div className="Fixed-bottom">
+        <FooterNav />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
