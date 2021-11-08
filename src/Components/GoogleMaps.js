b479c30465f72be3e13ef0ca63c1ffcf570cd9c9
Ridhwan Chowdhury <ridhwanchowdhury@Ridhwans-MacBook-Pro.local>
2021-11-08 00:09:20 -0500

    added info window and links

--- src/Components/GoogleMaps.js
+++ src/Components/GoogleMaps.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import env from "react-dotenv";
 
 const containerStyle = {
   position: 'relative',
@@ -9,7 +8,7 @@ const containerStyle = {
   }
 
 export class MapContainer extends Component {
-  /* Describes the initial state of the map when it's rendered */
+/* Describes the initial state of the map when it's rendered */
     state = {
       showingInfoWindow: false,
       activeMarker: {},
@@ -19,27 +18,28 @@ export class MapContainer extends Component {
           lng: -79.233238
       }
     };
-   
+ /* Event Handlers */
     onMarkerClick = (props, marker, e) =>
       this.setState({
         selectedPlace: props,
         activeMarker: marker,
         showingInfoWindow: true
       });
-   
     onMapClicked = (props) => {
       if (this.state.showingInfoWindow) {
         this.setState({
-          showingInfoWindow: true,
+          showingInfoWindow: false,
           activeMarker: null
         })
       }
     };
-   
+
     render() {
       return (
         <Map 
-            google={this.props.google}
+            google = {this.props.google}
+            onClick = {this.onMapClicked}
+            zoom = {13}
             initialCenter = {{
                 lat: this.state.mapCenter.lat,
                 lng: this.state.mapCenter.lng
@@ -51,36 +51,61 @@ export class MapContainer extends Component {
             containerStyle = {containerStyle}
             >
           <Marker 
+            onClick={this.onMarkerClick}
+            name={'Fit4Less'}
             position = {{
-                lat: this.state.mapCenter.lat,
-                lng: this.state.mapCenter.lng  
+                lat: 43.76187244422863, 
+                lng: -79.22827111828352 
             }}
-            />
-          <Marker 
-            position = {{
+          />
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+                  <a href = "./Item"> Click here for more information </a>
+              </div>
+            </InfoWindow>
+          <Marker
+              onClick={this.onMarkerClick}
+              name={'Goodlife Fitness: Cedarbrae Mall'} 
+              position = {{
                 lat: 43.75957,
                 lng: -79.22624
             }}
           />
           <Marker 
+            onClick={this.onMarkerClick}
+            name={'World Gym'}
             position = {{
                 lat: 43.77714113246414, 
                 lng: -79.24931225786676
             }}
           />
-          <Marker 
+          <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+                  <a href = "./Item"> Click here for more information </a>
+              </div>
+            </InfoWindow>
+          <Marker
+          onClick={this.onMarkerClick}
+          name={'Snap Fitness'} 
             position = {{
                 lat: 43.80145785954532,
                 lng:  -79.19827247757445
             }}
           />
           <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
-            <div>
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+              <div>
                 <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-          </InfoWindow>
+                  <a href = "./Item"> Click here for more information </a>
+              </div>
+            </InfoWindow>
         </Map>
       )
     }
